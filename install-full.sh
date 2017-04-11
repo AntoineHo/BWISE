cfd32b1f0fdd35cba011b3fc5edee925a6fc5a45
Patrick Durand <patrick.durand@inria.fr>
2017-04-11 09:56:10 +0200

    fix comments; clean after processing

--- install-full.sh
+++ install-full.sh
@@ -54,94 +54,99 @@ if [ -z "$BUILD_DIR"  ]; then
 	fi
 
 # make the build directory if it does not exist
-mkdir -p $BUILD_DIR;
-echo "INFO: all binaries will be placed in: $BUILD_DIR";
+mkdir -p $BUILD_DIR
+echo "INFO: all binaries will be placed in: $BUILD_DIR"
 
 # switch to source directory and make the entire build: BWISE and all
 # its dependencies
-cd ${SCRIPT_DIR}/src;
+cd ${SCRIPT_DIR}/src
 
 echo "INFO: compiling BWISE..."
-make LOL=-Dfolder=$BUILD_DIR -j $threadNumber >>logCompile 2>>logCompile;
-cp bwise ..;
-cp K2000/*.py $BUILD_DIR;
-cp sequencesToNumbers $BUILD_DIR;
-cp numbersFilter $BUILD_DIR;
-cp numbersToSequences $BUILD_DIR;
+make LOL=-Dfolder=$BUILD_DIR -j $threadNumber >>logCompile 2>>logCompile
+cp bwise ..
+cp K2000/*.py $BUILD_DIR
+cp sequencesToNumbers $BUILD_DIR
+cp numbersFilter $BUILD_DIR
+cp numbersToSequences $BUILD_DIR
 cp n50 $BUILD_DIR
 
 echo ""
 echo "INFO: cloning Bloocoo..."
-git clone --recursive https://github.com/GATB/bloocoo.git >>logCompile 2>>logCompile;
-cd bloocoo;
+git clone --recursive https://github.com/GATB/bloocoo.git >>logCompile 2>>logCompile
+cd bloocoo
 echo "INFO: compiling Bloocoo-32..."
-mkdir build32; cd build32;
-cmake -DKSIZE_LIST="32" .. >>logCompile 2>>logCompile;
-make -j $threadNumber >>logCompile 2>>logCompile;
-cp bin/Bloocoo Bloocoo32;
-cp Bloocoo32 $BUILD_DIR;
-cd ..;
+mkdir build32; cd build32
+cmake -DKSIZE_LIST="32" .. >>logCompile 2>>logCompile
+make -j $threadNumber >>logCompile 2>>logCompile
+cp bin/Bloocoo Bloocoo32
+cp Bloocoo32 $BUILD_DIR
+cd ..
 echo "INFO: compiling Bloocoo-64..."
-mkdir build64; cd build64;
-cmake -DKSIZE_LIST="64" .. >>logCompile 2>>logCompile;
-make -j $threadNumber >>logCompile 2>>logCompile;
-cp bin/Bloocoo Bloocoo64;
-cp Bloocoo64 $BUILD_DIR;
-cd ..;
+mkdir build64; cd build64
+cmake -DKSIZE_LIST="64" .. >>logCompile 2>>logCompile
+make -j $threadNumber >>logCompile 2>>logCompile
+cp bin/Bloocoo Bloocoo64
+cp Bloocoo64 $BUILD_DIR
+cd ..
 echo "INFO: compiling Bloocoo-128..."
-mkdir build128; cd build128;
-cmake -DKSIZE_LIST="128" .. >>logCompile 2>>logCompile;
-make -j $threadNumber >>logCompile 2>>logCompile;
-cp bin/Bloocoo Bloocoo128;
-cp Bloocoo128 $BUILD_DIR;
-cd ../..;
-#~ cp bloocoo/build32/ext/gatb-core/bin/h5dump $BUILD_DIR;
+mkdir build128; cd build128
+cmake -DKSIZE_LIST="128" .. >>logCompile 2>>logCompile
+make -j $threadNumber >>logCompile 2>>logCompile
+cp bin/Bloocoo Bloocoo128
+cp Bloocoo128 $BUILD_DIR
+cd ../..
+#~ cp bloocoo/build32/ext/gatb-core/bin/h5dump $BUILD_DIR
 
 echo ""
 echo "INFO: cloning Bclam..."
-git clone --recursive https://github.com/GATB/bcalm >>logCompile 2>>logCompile;
-cd bcalm;
+git clone --recursive https://github.com/GATB/bcalm >>logCompile 2>>logCompile
+cd bcalm
 echo "INFO: compiling Bcalm-32 64 128 160 224 256 320 512 1024..."
-mkdir build; cd build;
-cmake -DKSIZE_LIST="32 64 128 160 224 256 320 512 1024"  ..  >>logCompile 2>>logCompile;
-make -j $threadNumber >>logCompile 2>>logCompile;
-cp bcalm $BUILD_DIR;
-cd ../..;
+mkdir build; cd build
+cmake -DKSIZE_LIST="32 64 128 160 224 256 320 512 1024"  ..  >>logCompile 2>>logCompile
+make -j $threadNumber >>logCompile 2>>logCompile
+cp bcalm $BUILD_DIR
+cd ../..
 
 echo ""
 echo "INFO: cloning BGREAT2..."
-git clone https://github.com/Malfoy/BGREAT2 >>logCompile 2>>logCompile;
-cd BGREAT2;
+git clone https://github.com/Malfoy/BGREAT2 >>logCompile 2>>logCompile
+cd BGREAT2
 echo "INFO: compiling BGREAT2..."
-make -j $threadNumber >>logCompile 2>>logCompile;
-cp bgreat $BUILD_DIR;
-cd ..;
+make -j $threadNumber >>logCompile 2>>logCompile
+cp bgreat $BUILD_DIR
+cd ..
 
-#~ git clone --recursive https://github.com/Malfoy/BREADY >>logCompile 2>>logCompile;
-#~ cd BREADY;
-#~ mkdir build; cd build;
-#~ cmake .. >>logCompile 2>>logCompile;
-#~ make -j $threadNumber >>logCompile 2>>logCompile;
-#~ cp bin/BREADY $BUILD_DIR;
-#~ cd ../..;
-#~ git clone --recursive https://github.com/GATB/dsk.git >>logCompile 2>>logCompile;
-#~ cd dsk;
-#~ mkdir build;  cd build;
-#~ cmake -DKSIZE_LIST="32" .. >>logCompile 2>>logCompile;
-#~ make -j $threadNumber >>logCompile 2>>logCompile;
-#~ cp bin/dsk $BUILD_DIR;
-#~ cd ../..;
-#~ echo PHASE FOUR, SUPERREADS CLEANING: BREADY;
+#~ git clone --recursive https://github.com/Malfoy/BREADY >>logCompile 2>>logCompile
+#~ cd BREADY
+#~ mkdir build; cd build
+#~ cmake .. >>logCompile 2>>logCompile
+#~ make -j $threadNumber >>logCompile 2>>logCompile
+#~ cp bin/BREADY $BUILD_DIR
+#~ cd ../..
+#~ git clone --recursive https://github.com/GATB/dsk.git >>logCompile 2>>logCompile
+#~ cd dsk
+#~ mkdir build;  cd build
+#~ cmake -DKSIZE_LIST="32" .. >>logCompile 2>>logCompile
+#~ make -j $threadNumber >>logCompile 2>>logCompile
+#~ cp bin/dsk $BUILD_DIR
+#~ cd ../..
+#~ echo PHASE FOUR, SUPERREADS CLEANING: BREADY
 
 echo ""
 echo "INFO: cloning kMILL..."
-git clone https://github.com/kamimrcht/kMILL >>logCompile 2>>logCompile;
-cd kMILL/src;
+git clone https://github.com/kamimrcht/kMILL >>logCompile 2>>logCompile
+cd kMILL/src
 echo "INFO: compiling kMILL..."
-make -j $threadNumber >>logCompile 2>>logCompile;
-cp kMILL $BUILD_DIR;
-cp sequencesCleaner $BUILD_DIR;
-cp tipCleaner $BUILD_DIR;
-cd ../..;
+make -j $threadNumber >>logCompile 2>>logCompile
+cp kMILL $BUILD_DIR
+cp sequencesCleaner $BUILD_DIR
+cp tipCleaner $BUILD_DIR
 
 echo "DONE: 'bwise' binary available in $SCRIPT_FOLDER"
+
+# Clean project
+cd ${SCRIPT_DIR}/src
+make clean
+rm -rf BGREAT2 bcalm bloocoo kMILL
+
